903f5ac935657ac6deac445cad50847295438c2e
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-10 10:31:33 -0700

    Add changeColor function to javascript.js

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ const defaultDimension = 16;
 
 addGrid(defaultDimension);
 
+
 function addGrid(dimension) {
     let rowCount = dimension;
     while (rowCount > 0) {
@@ -11,9 +12,14 @@ function addGrid(dimension) {
         for (let i = 0; i < dimension; i++) {
             const square = document.createElement("div");
             square.setAttribute("class", "square");
+            square.addEventListener("mouseover", changeColor);
             row.appendChild(square);
         } 
         container.appendChild(row);
         rowCount--;
     }
+}
+
+function changeColor(event) {
+    event.target.style.backgroundColor = "black";
 }
